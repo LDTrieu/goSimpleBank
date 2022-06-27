ff584136ba5d59b551c995a004052b4af58718b3
ldtrieu <letrieu106@gmail.com>
2022-06-27 23:05:34 +0700

    add Dockerfile

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"database/sql"
-	"log"
+	"fmt"
 
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-
-	"github.com/LDTrieu/goSimpleBank/api"
-	db "github.com/LDTrieu/goSimpleBank/db/sqlc"
-	"github.com/LDTrieu/goSimpleBank/util"
 )
 
 func main() {
-	config, err := util.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
+	fmt.Println("Test")
+	/*
+		config, err := util.LoadConfig(".")
+		if err != nil {
+			log.Fatal("cannot load config:", err)
+		}
 
-	conn, err := sql.Open(config.DBDriver, config.DBSource)
-	if err != nil {
-		log.Fatal("cannot connect to db:", err)
-	}
-	store := db.NewStore(conn)
-	server := api.NewServer(store)
 
-	err = server.Start(config.HTTPServerAddress)
-	if err != nil {
-		log.Fatal("cannot start server:", err)
-	}
+		conn, err := sql.Open(config.DBDriver, config.DBSource)
+		if err != nil {
+			log.Fatal("cannot connect to db:", err)
+		}
+
+			store := db.NewStore(conn)
+			server := api.NewServer(store)
+
+			err = server.Start(config.HTTPServerAddress)
+			if err != nil {
+				log.Fatal("cannot start server:", err)
+			}
+	*/
 
 }
